3af6cb9735eda949b9792ab9997b3a9ab20316ec
alancodex <alansobraljs@gmail.com>
2025-10-26 12:21:55 -0300

    AuthControllerTest

--- finansee/src/test/java/com/controle/finansee/FinanseeApplicationTests.java
+++ finansee/src/test/java/com/controle/finansee/FinanseeApplicationTests.java
@@ -2,15 +2,12 @@ package com.controle.finansee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test") // 🔹 ativa o perfil de teste
 class FinanseeApplicationTests {
 
-    @Test
-    void contextLoads() {
-        System.out.println("✅ Contexto carregado com sucesso!");
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
